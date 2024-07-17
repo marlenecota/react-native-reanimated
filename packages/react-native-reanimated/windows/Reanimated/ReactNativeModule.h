9694c283bf0fcd5dff20455be044d11c1410754e
Marlene Cota <1422161+marlenecota@users.noreply.github.com>
2024-07-16 21:33:38 -0700

    add windows lib template

---
+++ packages/react-native-reanimated/windows/Reanimated/ReactNativeModule.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "JSValue.h"
+#include "NativeModules.h"
+
+using namespace winrt::Microsoft::ReactNative;
+
+namespace winrt::Reanimated
+{
+
+REACT_MODULE(ReactNativeModule, L"ReanimatedModule")
+struct ReactNativeModule
+{
+  REACT_SYNC_METHOD(installTurboModule)
+  bool installTurboModule(std::string valueUnpackerCode) noexcept {
+    return false;
+  }
+};
+
+} // namespace winrt::Reanimated
